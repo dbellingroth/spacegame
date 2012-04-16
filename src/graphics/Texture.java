352fec4d79e2649e8a9c8806a8fda3a3fd94007b
David Bellingroth <dbellingroth@gmail.com>
2012-04-17 00:05:45 +0200

    kompilierte Klassen werden ignoriert

--- src/graphics/Texture.java
+++ src/graphics/Texture.java
@@ -1,5 +1,6 @@
 package graphics;
 
+
 public class Texture {
 
 }
--- src/graphics/Window.java
+++ src/graphics/Window.java
@@ -1,5 +1,6 @@
 package graphics;
 
+
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
