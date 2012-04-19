c5519c3ed7afa3c08936dcbcfb177e7a6f4e4db2
Kilian Helmenstein <kily100@aol.com>
2012-04-19 18:24:38 +0200

    Leerzeichen eingefügt

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,17 @@ public class Main {
 	 * @throws InterruptedException 
 	 */
 	public static void main(String[] args) throws InterruptedException {
-		Window w = new Window(640,480);
+		
+		Window w = new Window( 640,480 );
 		FPSCounter.tick();
+		
 		while (!w.shouldClose()) {
-			w.setTitle("FPS: "+FPSCounter.getFPS()+ " Delta: "+FPSCounter.getDelta());
-			Thread.sleep(500);
+			
+			w.setTitle( "FPS: "+FPSCounter.getFPS()+ " Delta: "+FPSCounter.getDelta() );
+			Thread.sleep( 500 );
 			FPSCounter.tick();
 		}
+		
 		w.close();
 	}
 
